3920dcc480fb38e41cb9858fb12791389ea1905d
saifi <saifi@strikr.io>
2021-05-17 11:12:44 +0100

    added kube-controlled-manager scripts for json and certificate generation
          do_cert_kube_controller_manager_gen.sh
          do_cert_kube_controller_manager_json.sh

---
+++ scripts/kube-controller-manager/do_cert_kube_controller_manager_json.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+
+NODE_NAME="kube-controller-manager"
+
+
+cat > ${NODE_NAME}_csr.json <<EOF
+{
+      "CN" : "system:${NODE_NAME}",
+      "Key": {
+             "algo" : "rsa",
+             "size" : 2048
+      },
+      "names" : [
+            {
+                  "C": "IN",
+                  "L": "Bengaluru",
+                  "O": "system:${NODE_NAME}",
+                  "OU": "Strikr",
+                  "ST": "Karnataka"
+            }
+      ]
+}
+EOF
+
+
+# STRIKR commentary
+#
